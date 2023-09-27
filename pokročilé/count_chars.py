380c0d7b48bca9eaeff602fe2321d2c41d9ce576
dlabaja <dlabaja.jan@seznam.cz>
2023-09-27 14:09:16 +0200

    count_chars formatting + stdin

--- pokročilé/count_chars.py
+++ pokročilé/count_chars.py
@@ -1,26 +1,30 @@
 from string import ascii_lowercase
+from sys import stdin, stdout, stderr
 import unicodedata
 import math
 
 def percent_to_text(percent):
-    return "#" * round(percent / 2)
+    return "#" * percent
+
+def sum_values(char_dict):
+    result = 0
+    for i in char_dict.values():
+        result += i
+    return result
 
 # input = unicodedata.normalize("NFKD", input("Zadejte vstup: ")).encode("ascii", "ignore").decode("ascii")
-input = "sussy suspppp"
+input = unicodedata.normalize("NFKD", stdin.read()).encode("ascii", "ignore").decode("ascii")
 
 chars = {}
-char_count = 0
 
+# init chars dict
 for i in ascii_lowercase:
     chars[i] = 0
 
 for i in input:
-    try:
+    if i in chars:
         chars[i] += 1
-        char_count += 1
-    except KeyError:
-        continue
 
-for k, v in sorted(chars.items(), key=lambda x:x[1], reverse=True):
-    print(f"{k}: {v} ({round(v/char_count*100):2d} %) {percent_to_text(round(v/char_count*100))}")
-
+sum = sum_values(chars)
+for k, v in chars.items(): # sorted(chars.items(), key=lambda x:x[1], reverse=True):
+    print(f"{k}: {v:{len(str(sum))}d} ({v/sum*100:5.1f} %) {percent_to_text(round(v/sum*100))}") # 'a:  74 (  7.6 %) ########'
