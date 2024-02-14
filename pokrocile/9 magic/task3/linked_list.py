4dca8bf57bb6cdc16213b6edc097a665b7027884
dlabaja <dlabaja.jan@seznam.cz>
2024-02-14 21:57:52 +0100

    základ linked listu funguje, teď jdu pohlídat indexy

--- pokrocile/9 magic/task3/linked_list.py
+++ pokrocile/9 magic/task3/linked_list.py
@@ -1,13 +1,86 @@
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, prev_node=None, data=None, next_node=None):
         self.data = data
-        self.next_node = None
-        self.prev_node = None
+        self.next_node = next_node
+        self.prev_node = prev_node
 
-
-class DoublyLinkedList():
+class DoublyLinkedList:
     def __init__(self):
         super().__init__()
-        self.head = None
-        self.tail = None
+        self.head: Node = None
+        self.tail: Node = None
         self.length = 0
+
+    def append(self, data):
+        if len(self) == 0:
+            node = Node(None, data, None)
+            self.head = node
+            self.tail = node
+            self.length += 1
+            return
+
+        node = Node(self.tail, data, None)
+        self.tail.next_node = node
+        self.tail = node
+        self.length += 1
+
+    def insert(self, data, index):
+        # todo záporné indexy
+        if index > self.length:
+            raise IndexError
+
+        if index == self.length:  # na konci
+            self.append(data)
+            return
+
+        if index == 0:  # na začátku
+            if len(self) == 0:
+                self.append(data)
+                return
+
+            node = Node(None, data, self.head)
+            self.head.prev_node = node
+            self.head = node
+            self.length += 1
+            return
+
+        prev_node = self.get_node(index - 1)
+        node = Node(prev_node, data, prev_node.next_node)
+        prev_node.next_node.prev_node = node
+        prev_node.next_node = node
+        self.length += 1
+
+    def __iter__(self):
+        ls = [self.head.data]
+        last = self.head
+        for _ in range(len(self) - 1):
+            ls.append(last.next_node.data)
+            last = last.next_node
+
+        return iter(ls)
+
+    def get_node(self, index) -> Node:
+        last = self.head
+        if index == 0:
+            return last
+
+        for i in range(len(self) - 1):
+            if i == index - 1:
+                return last.next_node
+            last = last.next_node
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        return list(self)[index]
+
+    def __setitem__(self, index, data):
+        self.get_node(index).data = data
+
+    def __delitem__(self, index):
+        node = self.get_node(index)
+        prev = node.prev_node
+        node.prev_node.next_node = node.next_node
+        node.next_node.prev_node = prev
+        self.length -= 1
--- pokrocile/9 magic/task3/test_linked_list.py
+++ pokrocile/9 magic/task3/test_linked_list.py
@@ -1,3 +1,5 @@
+import pytest
+
 from linked_list import DoublyLinkedList
 
 
@@ -8,12 +10,14 @@ def test_length():
     linked_list.append(3)
     assert len(linked_list) == 3
 
+
 def test_get_item():
     linked_list = DoublyLinkedList()
     linked_list.append(1)
     linked_list.append(2)
     linked_list.append(3)
     assert linked_list[0] == 1
+
 
 def test_set_item():
     linked_list = DoublyLinkedList()
@@ -23,6 +27,7 @@ def test_set_item():
     linked_list[0] = 0
     assert linked_list[0] == 0
 
+
 def test_del_item():
     linked_list = DoublyLinkedList()
     linked_list.append(1)
@@ -31,6 +36,8 @@ def test_del_item():
     del linked_list[1]
     assert len(linked_list) == 2
     assert linked_list[1] == 3
+    assert linked_list.tail.data == 3
+
 
 def test_iteration():
     linked_list = DoublyLinkedList()
@@ -39,3 +46,38 @@ def test_iteration():
     linked_list.append(3)
     items = list(linked_list)
     assert items == [1, 2, 3]
+
+
+@pytest.fixture
+def sample_doubly_linked_list():
+    dll = DoublyLinkedList()
+    dll.insert(1, 0)
+    dll.insert(2, 1)
+    dll.insert(3, 2)
+    return dll
+
+
+def test_insert_at_beginning(sample_doubly_linked_list):
+    sample_doubly_linked_list.insert(0, 0)
+    assert sample_doubly_linked_list.head.data == 0
+    assert sample_doubly_linked_list.head.next_node.data == 1
+
+
+def test_insert_at_middle(sample_doubly_linked_list):
+    sample_doubly_linked_list.insert(4, 2)
+    assert sample_doubly_linked_list.head.next_node.next_node.data == 4
+    assert sample_doubly_linked_list.head.next_node.next_node.next_node.data == 3
+
+
+def test_insert_at_end(sample_doubly_linked_list):
+    sample_doubly_linked_list.insert(5, 3)
+    current = sample_doubly_linked_list.head
+    while current.next_node:
+        current = current.next_node
+    assert current.data == 5
+    assert current.prev_node.data == 3
+
+
+def test_insert_invalid_position(sample_doubly_linked_list):
+    with pytest.raises(IndexError):
+        sample_doubly_linked_list.insert(99, 5)
