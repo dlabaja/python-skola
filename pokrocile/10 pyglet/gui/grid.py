130da6208dd2c1dc56be5096c5538c03ee2349a1
dlabaja <dlabaja.jan@seznam.cz>
2024-02-22 22:48:00 +0100

    grid pain

--- pokrocile/10 pyglet/gui/grid.py
+++ pokrocile/10 pyglet/gui/grid.py
@@ -1,8 +1,12 @@
+import random
+
 import pyglet
 from pyglet import shapes
 from main_game.game import Game
 
 class Grid:
+    subRectOffset = 10
+
     def __init__(self, length, x, y):
         self.length = length
         self.anchor_x = x
@@ -16,19 +20,27 @@ class Grid:
 
     def draw_cell(self, x, y):
         # main_game rect
-        r = shapes.Rectangle(x=self.anchor_x - x * self.cell_size, y=self.anchor_y - y * self.cell_size,
-                             width=self.cell_size, height=self.cell_size,
+        r = shapes.Rectangle(x=self.anchor_x - x * self.cell_size,
+                             y=self.anchor_y - y * self.cell_size,
+                             width=self.cell_size,
+                             height=self.cell_size,
                              color=(255, 255, 255))
         r.anchor_x = self.cell_size
         r.anchor_y = self.cell_size
         r.draw()
 
+        self.draw_inner_cell(x, y)
+
+    def draw_inner_cell(self, x, y):
         # sub rect
-        sr = shapes.Rectangle(x=self.anchor_x - x * self.cell_size, y=self.anchor_y - y * self.cell_size,
-                             width=self.cell_size * 0.8, height=self.cell_size * 0.8,
-                             color=(55, 55, 255))
-        sr.anchor_x = self.cell_size * 0.8
-        sr.anchor_y = self.cell_size * 0.8
+        sr = shapes.Rectangle(x=self.anchor_x - x * self.cell_size,
+                              y=self.anchor_y - y * self.cell_size,
+                              # (4 * self.cell_size - 5 * Grid.subRectOffset) // 4
+                              width=self.cell_size - 2 * Grid.subRectOffset,
+                              height=self.cell_size - 2 * Grid.subRectOffset,
+                              color=(55, 55, 255))
+        sr.anchor_x = self.cell_size - Grid.subRectOffset
+        sr.anchor_y = self.cell_size - Grid.subRectOffset
         sr.draw()
 
         label = pyglet.text.Label(str(Game.current.get_block_value(x, y)), font_size=20,
