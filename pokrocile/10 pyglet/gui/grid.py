8608a806b7683eb4368aac32d1985b4559e3fd99
dlabaja <dlabaja.jan@seznam.cz>
2024-02-23 21:44:15 +0100

    ui a barvicky

--- pokrocile/10 pyglet/gui/grid.py
+++ pokrocile/10 pyglet/gui/grid.py
@@ -4,8 +4,22 @@ import pyglet
 from pyglet import shapes
 from main_game.game import Game
 
+
 class Grid:
     subRectOffset = 10
+    tile_colors = {  # hodnota : (fg, bg)
+        2: ((119, 110, 101), (238, 228, 218)),
+        4: ((119, 110, 101), (237, 224, 200)),
+        8: ((249, 246, 242), (242, 177, 121)),
+        16: ((249, 246, 242), (245, 149, 99)),
+        32: ((249, 246, 242), (246, 124, 95)),
+        64: ((249, 246, 242), (246, 94, 59)),
+        128: ((249, 246, 242), (237, 207, 114)),
+        256: ((249, 246, 242), (237, 204, 97)),
+        512: ((249, 246, 242), (237, 200, 80)),
+        1024: ((249, 246, 242), (237, 197, 63)),
+        2048: ((249, 246, 242), (237, 194, 46))
+    }
 
     def __init__(self, length, x, y):
         self.length = length
@@ -20,30 +34,49 @@ class Grid:
 
     def draw_cell(self, x, y):
         # main_game rect
-        r = shapes.Rectangle(x=self.anchor_x - x * self.cell_size,
+        s = shapes.Rectangle(x=self.anchor_x - x * self.cell_size,
                              y=self.anchor_y - y * self.cell_size,
                              width=self.cell_size,
                              height=self.cell_size,
-                             color=(255, 255, 255))
-        r.anchor_x = self.cell_size
-        r.anchor_y = self.cell_size
-        r.draw()
+                             color=(187, 173, 160))
 
-        self.draw_inner_cell(x, y)
+        s.anchor_x = self.cell_size
+        s.anchor_y = self.cell_size
+        s.draw()
 
-    def draw_inner_cell(self, x, y):
+        self.draw_inner_cell_placeholder(x, y)
+
+    def draw_inner_cell_placeholder(self, x, y):
         # sub rect
-        sr = shapes.Rectangle(x=self.anchor_x - x * self.cell_size,
-                              y=self.anchor_y - y * self.cell_size,
-                              # (4 * self.cell_size - 5 * Grid.subRectOffset) // 4
-                              width=self.cell_size - 2 * Grid.subRectOffset,
-                              height=self.cell_size - 2 * Grid.subRectOffset,
-                              color=(55, 55, 255))
-        sr.anchor_x = self.cell_size - Grid.subRectOffset
-        sr.anchor_y = self.cell_size - Grid.subRectOffset
-        sr.draw()
+        s = shapes.Rectangle(x=self.anchor_x - x * self.cell_size,
+                             y=self.anchor_y - y * self.cell_size,
+                             width=self.cell_size - 2 * Grid.subRectOffset,
+                             height=self.cell_size - 2 * Grid.subRectOffset,
+                             color=(205, 193, 180))
 
-        label = pyglet.text.Label(str(Game.current.get_block_value(x, y)), font_size=20,
-                                  x=self.anchor_x - self.cell_size // 2,  # Center horizontally
-                                  y=self.anchor_y - self.cell_size // 2)  # Center vertically
-        label.draw()
+        s.anchor_x = self.cell_size - Grid.subRectOffset
+        s.anchor_y = self.cell_size - Grid.subRectOffset
+        s.draw()
+
+        # str(Game.current.get_block_value(x, y)
+        self.draw_text_cell(x, y, str(999999))
+
+    def draw_text_cell(self, x, y, text):
+        s = shapes.Rectangle(x=self.anchor_x - x * self.cell_size,
+                             y=self.anchor_y - y * self.cell_size,
+                             width=self.cell_size - 2 * Grid.subRectOffset,
+                             height=self.cell_size - 2 * Grid.subRectOffset,
+                             color=(205, 193, 180))
+
+        s.anchor_x = self.cell_size - Grid.subRectOffset
+        s.anchor_y = self.cell_size - Grid.subRectOffset
+        s.draw()
+
+        pyglet.text.Label(text, font_size=32 - 2 * (len(text)),
+                          x=self.anchor_x - x * self.cell_size - self.cell_size // 2,
+                          y=self.anchor_y - y * self.cell_size - self.cell_size + self.cell_size // 4,
+                          width=self.cell_size - 2 * Grid.subRectOffset,
+                          height=self.cell_size - 2 * Grid.subRectOffset,
+                          color=(0, 0, 0, 255),
+                          anchor_x='center',
+                          anchor_y='center').draw()
--- pokrocile/10 pyglet/gui/renderer.py
+++ pokrocile/10 pyglet/gui/renderer.py
@@ -1,5 +1,8 @@
 import pyglet
+from pyglet import shapes, gl
+
 from gui.grid import Grid
+
 
 class Renderer:
     grid = None
@@ -11,8 +14,10 @@ class Renderer:
     @staticmethod
     def initialize(width, height, title):
         r = Renderer
+
         r.window = pyglet.window.Window(width=width, height=height, caption=title)
-        r.window.event(r.on_draw)
+        r.window.on_draw = r.on_draw
+        r.window.set_minimum_size(800, 600)
 
         size = r.calculate_grid_size()
         r.grid = Grid(size, r.window.width - r.x_offset, r.window.height - r.y_offset)
@@ -22,7 +27,16 @@ class Renderer:
     @staticmethod
     def on_draw():
         Renderer.window.clear()
+        Renderer.draw_window()
         Renderer.grid.redraw_grid()
+
+    @staticmethod
+    def draw_window():
+        shapes.Rectangle(x=0,
+                         y=0,
+                         width=Renderer.window.width,
+                         height=Renderer.window.width,
+                         color=(250, 248, 239)).draw()
 
     @staticmethod
     def calculate_grid_size():
