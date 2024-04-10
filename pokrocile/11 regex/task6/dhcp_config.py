980f325a9978e83d6d33436536d2498a30fdccb9
dlabaja <dlabaja.jan@seznam.cz>
2024-04-10 15:56:25 +0200

    regex task6

--- pokrocile/11 regex/task6/dhcp_config.py
+++ pokrocile/11 regex/task6/dhcp_config.py
@@ -6,11 +6,6 @@ def check_dhcp_config(line: str) -> bool:
 
 
 def transfer_dhcp_config(text: str) -> str:
-    text = """
-host cosi.kdesi.cz {
-  link address 00:40:38:9a:5b:60;
-  fix  address 192.51.38.444;
-}
-"""
-    return re.sub(r"([a-z]*\.[a-z]*\.[a-z]*)\s*ha=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}):ip=(\d+\.\d+\.\d+\.\d+)",
-                  "host \1 {\nlink address \2:\3:\4:\5:\6:\7;\n fix  address \8;", text)
+    return re.sub(
+        r"([a-z]*\.[a-z]*\.[a-z]*)\s*ha=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}):ip=(\d+\.\d+\.\d+\.\d+)",
+        r"\nhost \1 {\nlink address \2:\3:\4:\5:\6:\7;\n fix  address \8;\n}\n", text)
