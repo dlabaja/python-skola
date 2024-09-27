bde4df81aa5fc26aa8d1b7feafb7879ab301ae9f
dlabaja <dlabaja.jan@seznam.cz>
2024-09-27 14:05:32 +0200

    tkinter

---
+++ pokrocile/13 tkinter/README.md
@@ -0,0 +1,53 @@
+PRG -- TkInter start
+========================
+
+[![Join the chat at gitter.im/spseol/Python](https://badges.gitter.im/spseol/PRG-No.svg)](https://gitter.im/spseol/Python?utm_source=share-link&utm_medium=link&utm_campaign=share-link)
+
+
+*  [main.py](main.py)
+
+Dokumentace
+-------------
+
+* <https://tkdocs.com/shipman/>
+* <http://tkinter.programujte.com/>
+
+Rapid application development
+-------------------------------
+
+* [Pygubu](https://github.com/alejandroautalan/pygubu-designer)
+
+Co je třeba k psaní programů v TkInter
+==========================================
+
+1. Widgety: 
+  * co mají společné: http://tkinter.programujte.com/widget.htm
+  * co mají společné: https://tkdocs.com/shipman/std-attrs.html
+  * jednotlivě: http://tkinter.programujte.com/#class-reference
+  * jednotlivě: https://tkdocs.com/shipman/
+
+2. Roznístění:
+  * http://tkinter.programujte.com/pack.htm
+  * http://tkinter.programujte.com/place.htm
+  * http://tkinter.programujte.com/grid.htm
+  * https://tkdocs.com/shipman/layout-mgt.html
+
+3. Události a vazby:
+  * http://tkinter.programujte.com/tkinter-events-and-bindings.htm
+  * https://tkdocs.com/shipman/events.html
+
+4. Obrázky
+  * https://blog.furas.pl/python-tkinter-how-to-load-display-and-replace-image-on-label-button-or-canvas-gb.html
+  * https://pythonbasics.org/tkinter-image/ 
+
+5. Aplikace s více okny
+  * https://www.pythontutorial.net/tkinter/tkinter-toplevel/
+  * https://www.pythontutorial.net/tkinter/tkinter-frame/
+ 
+6. Validace vstupu
+  * https://www.pythontutorial.net/tkinter/tkinter-validation/
+  * https://tkdocs.com/shipman/entry-validation.html
+  * https://www.geeksforgeeks.org/python-tkinter-validating-entry-widget/
+  
+7. Pořadí widgetů při přepínání tabulátorem
+   * https://www.tutorialspoint.com/how-to-set-the-tab-order-in-a-tkinter-application
---
+++ pokrocile/13 tkinter/main.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+from tkinter import HORIZONTAL, TOP, LEFT
+
+# from tkinter import ttk
+
+
+class MyEntry(tk.Entry):
+    def __init__(self, master=None, cnf={}, **kw):
+        super().__init__(master, cnf, **kw)
+
+        if "textvariable" not in kw:
+            self.variable = tk.StringVar()
+            self.config(textvariable=self.variable)
+        else:
+            self.variable = kw["textvariable"]
+
+    @property
+    def value(self):
+        return self.variable.get()
+
+    @value.setter
+    def value(self, new: str):
+        self.variable.set(new)
+
+
+class Application(tk.Tk):
+    name = "Foo"
+
+    def __init__(self):
+        super().__init__(className=self.name)
+        self.title(self.name)
+        self.bind("<Escape>", self.quit)
+        self.lblMain = tk.Label(self, text="Color Mishmash")
+        self.btnQuit = tk.Button(self, text="Save & Quit", command=self.quit)
+        self.btnClear = tk.Button(self, text="Clear colors", command=self.clearColors)
+        self.frameR = tk.Frame(master=self)
+        self.frameG = tk.Frame(self)
+        self.frameB = tk.Frame(self)
+        self.frameMem = tk.Frame(self)
+
+        self.lblR = tk.Label(self.frameR, text="R", width=2)
+        self.lblG = tk.Label(self.frameG, text="G", width=2)
+        self.lblB = tk.Label(self.frameB, text="B", width=2)
+
+        self.varR = tk.IntVar(value=255)
+        self.varR.trace_add("write", self.updateColor)
+        self.scaleR = tk.Scale(
+            self.frameR,
+            from_=0,
+            to=255,
+            orient=HORIZONTAL,
+            length=300,
+            variable=self.varR,
+        )
+        self.entryR = tk.Entry(self.frameR, textvariable=self.varR, width=3)
+
+        self.varG = tk.IntVar(value=255)
+        self.varG.trace_add("write", self.updateColor)
+        self.scaleG = tk.Scale(
+            self.frameG,
+            from_=0,
+            to=255,
+            orient=HORIZONTAL,
+            length=300,
+            variable=self.varG,
+        )
+        self.entryG = tk.Entry(self.frameG, textvariable=self.varG, width=3)
+
+        self.varB = tk.IntVar(value=255)
+        self.varB.trace_add("write", self.updateColor)
+        self.scaleB = tk.Scale(
+            self.frameB,
+            from_=0,
+            to=255,
+            orient=HORIZONTAL,
+            length=300,
+            variable=self.varB,
+        )
+        self.entryB = tk.Entry(self.frameB, textvariable=self.varB, width=3)
+
+        self.canvas = tk.Canvas(self, background="#ffffff", width=30)
+        self.canvas.bind("<Button-1>", self.clickHandler)
+
+        self.lblMain.pack(side=TOP)
+        self.frameR.pack(side=TOP)
+        self.frameG.pack(side=TOP)
+        self.frameB.pack(side=TOP)
+
+        self.lblR.pack(side=LEFT, anchor="s")
+        self.scaleR.pack(side=LEFT, anchor="s")
+        self.entryR.pack(side=LEFT, anchor="s")
+
+        self.lblG.pack(side=LEFT, anchor="s")
+        self.scaleG.pack(side=LEFT, anchor="s")
+        self.entryG.pack(side=LEFT, anchor="s")
+
+        self.lblB.pack(side=LEFT, anchor="s")
+        self.scaleB.pack(side=LEFT, anchor="s")
+        self.entryB.pack(side=LEFT, anchor="s")
+
+        self.canvas.pack(side=TOP, fill="both")
+        self.frameMem.pack(side=TOP, fill="both")
+
+        self.canvasList = []
+        for row in range(3):
+            for col in range(7):
+                canvas = tk.Canvas(self.frameMem, width=50, height=50, bg="#ffffff")
+                canvas.bind("<Button-1>", self.clickHandler)
+                canvas.grid(row=row, column=col)
+                self.canvasList.append(canvas)
+
+        self.btnQuit.pack()
+        self.btnClear.pack()
+        self.load()
+
+    def clickHandler(self, event):
+        if self.cget("cursor") != "pencil":
+            self.config(cursor="pencil")
+            self.copycolor = event.widget.cget("background")
+        else:
+            self.config(cursor="")
+            if event.widget is self.canvas:
+                r = int(self.copycolor[1:3], 16)
+                g = int(self.copycolor[3:5], 16)
+                b = int(self.copycolor[5:], 16)
+                self.varR.set(r)
+                self.varG.set(g)
+                self.varB.set(b)
+            else:
+                event.widget.config(background=self.copycolor)
+
+    def updateColor(self, newvalue=None, neco=None, dalsi=None):
+        r = self.scaleR.get()
+        g = self.scaleG.get()
+        b = self.scaleB.get()
+        self.canvas.config(background=f"#{r:02X}{g:02X}{b:02X}")
+
+    def quit(self, event=None):
+        self.save()
+        super().quit()
+
+    def save(self):
+        colors = []
+        for canvas in self.canvasList:
+            colors.append(canvas.cget("background"))
+
+        file = open('latestColors.txt', 'w')
+        file.write(str.join("\n", colors))
+
+    def load(self):
+        file = open('latestColors.txt', 'r')
+        for i, color in enumerate(file.readlines()):
+            if self.canvasList[i]:
+                self.canvasList[i].config(background=str.replace(color, "\n", ""))
+
+    def clearColors(self):
+        for canvas in self.canvasList:
+            canvas.config(background="#FFFFFF")
+
+
+app = Application()
+app.mainloop()
